f348ee059c04f51326f43f98608b0c4d9db2944b
Ciobotariu Andrei <ciobotariuandrei@yahoo.com>
2021-05-10 18:54:39 +0300

    Revert "Reorganizare"
    This reverts commit adc9be96bc432c077dfbbfa8a6e5738e1562ab26.

--- src/main/java/ro/uaic/info/MailApplication/MailApplication.java
+++ src/main/java/ro/uaic/info/MailApplication/MailApplication.java
@@ -3,11 +3,10 @@ package ro.uaic.info.MailApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableEurekaClient
-@Configuration
+
 public class MailApplication {
 
     public static void main(String[] args) {
